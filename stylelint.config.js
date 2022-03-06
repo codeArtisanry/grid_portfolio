90e93c56bef36bd1c5380dfecbb44d6e3d0db1b6
Vatsal <chauhanvatsal55@gmail.com>
2022-03-06 09:47:46 +0530

    final project deployed

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   customSyntax: 'postcss-html',
   extends: [
-    'stylelint-config-standard',
-    'stylelint-config-recommended-vue',
     'stylelint-config-prettier',
   ],
   // add your custom config here
