01fd20e230de1ba18e70f5ff5aa3e3586457458b
Vatsal <chauhanvatsal55@gmail.com>
2022-03-06 09:02:32 +0530

    feat: basic format

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <grid-portfolio />
+</template>
+
+<script>
+import GridPortfolio from '~/components/GridPortfolio.vue'
+export default {
+  components: { GridPortfolio },
+}
+</script>
